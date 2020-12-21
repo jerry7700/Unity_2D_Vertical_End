1716455cd0067c88df9261b4ff4f98fa14b6338b
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2020-12-21 21:11:30 +0800

    主介面完成

---
+++ Unity_2D_Horizontal_End/Assets/everybody/script/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    Animator anim;
+
+    public float speed;
+    Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x !=0)
+        {
+            transform.localScale = new Vector3(-movement.x, 1, 1);
+        }
+        SiwitchAnim();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void SiwitchAnim()
+    {
+        anim.SetFloat("speed", movement.magnitude);
+    }
+}
---
+++ Unity_2D_Horizontal_End/Assets/everybody/script/projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float moveSpeed;
+
+    private float lifeTime;
+    [SerializeField] private float maxLife = 2.0f;
+
+    public GameObject destroyEffect;
+    public GameObject attackEffect;
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        lifeTime += Time.deltaTime;
+        if(lifeTime >= maxLife)
+        {
+            Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponentInChildren<HealthBar>().hp -= 25;//用於UI不能用GetComponent
+            Instantiate(attackEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
